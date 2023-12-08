7395c572f166cf108e463129925b2076875f03d6
dafolk <kaungsoesan01@gmail.com>
2023-12-08 16:12:10 +0630

    gitignore fixed and dotenv vault installed

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,33 @@ import OurVision from "../components/Home/OurVision";
 import Hero from "../components/Home/Hero";
 import AboutUs from "../components/Home/AboutUs";
 import OurValues from "../components/Home/OurValues";
+import { collection, doc, setDoc } from "firebase/firestore";
+import { firestoreDatabase } from "../utils/firebase";
 
 export default function HomePage() {
+  const logData = async () => {
+    // const querySnapshot = await getDocs(
+    //   collection(firestoreDatabase, "subscribers")
+    // );
+    // console.log(querySnapshot);
+    // querySnapshot.forEach((doc) => {
+    // console.log(`${doc.id} => ${doc.data()}`);
+    // console.log(doc);
+    // console.log(doc.data());
+    // });
+    const citiesRef = collection(firestoreDatabase, "cities");
+
+    await setDoc(doc(citiesRef, "SF"), {
+      name: "San Francisco",
+      state: "CA",
+      country: "MYANMAR",
+      capital: false,
+      population: 860000,
+      regions: ["west_coast", "norcal"],
+    });
+  };
+  logData();
+
   return (
     <Box
       width={"100%"}
